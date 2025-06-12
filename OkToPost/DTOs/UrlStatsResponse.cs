832ef38fb443809f3cc1a43a6f39d940934db11b
eyalank <eyalank@gmail.com>
2025-06-12 23:19:33 +0300

    Project files

---
+++ OkToPost/DTOs/UrlStatsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OkToPost.DTOs
+{
+    public class UrlStatsResponse
+    {
+        public string Code { get; set; }
+        public int Clicks { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
