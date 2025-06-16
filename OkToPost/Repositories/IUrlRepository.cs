96080936198371fb596573139ca3461d7e940df1
eyalank <eyalank@gmail.com>
2025-06-16 19:00:07 +0300

    Fix nullability issues in tests 4

--- OkToPost/Repositories/IUrlRepository.cs
+++ OkToPost/Repositories/IUrlRepository.cs
@@ -4,8 +4,8 @@ namespace OkToPost.Repositories
 {
     public interface IUrlRepository
     {
-        Task<UrlMapping> GetByCodeAsync(string code);
-        Task<UrlMapping> GetByUrlAsync(string url);
+        Task<UrlMapping?> GetByCodeAsync(string code);
+        Task<UrlMapping?> GetByUrlAsync(string url);
         Task AddAsync(UrlMapping mapping);
         Task IncrementClickCountAsync(string code);
     }
